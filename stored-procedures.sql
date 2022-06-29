b50bd69c019eca04403a3e31bcbdbb736984f2f6
Joshua Shirley <52868171+Joshua-Shirley@users.noreply.github.com>
2022-06-29 12:23:27 -0600

    Time Table update Procedure Added

--- stored-procedures.sql
+++ stored-procedures.sql
@@ -31,7 +31,6 @@ END;
 $$;
 
 
-
 CREATE OR REPLACE PROCEDURE public.updateDateTable
 (
     startDate DATE,
@@ -78,4 +77,35 @@ BEGIN
 END;
 $BODY$;
 
+CREATE OR REPLACE PROCEDURE public.updateTimeTable()
+LANGUAGE 'plpgsql'
+AS $BODY$
+BEGIN 
+    
+    /* Fill TimeFact with Data */
+    WITH RECURSIVE minutes(mi,s) AS (
+        SELECT 0, 0
+
+        UNION ALL
+
+        SELECT mi + 1, 0
+        FROM minutes
+        WHERE mi + 1 < 60
+    ), hours(h,mi,s) AS (    
+        SELECT 0 , mi, s
+        FROM minutes
+
+        UNION ALL
+
+        SELECT h + 1, mi, s
+        FROM hours
+        WHERE h + 1 < 24
+    )
+    INSERT INTO timefact
+    ( hour , minute , second )
+    SELECT *
+    FROM hours;
+    
+END;
+$BODY$
 
