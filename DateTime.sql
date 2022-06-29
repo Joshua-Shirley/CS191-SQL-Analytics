2c2e22e0d48721e25ad75ca815b037e7dd23f063
Joshua Shirley <52868171+Joshua-Shirley@users.noreply.github.com>
2022-06-28 22:51:37 -0600

    addition query

--- DateTime.sql
+++ DateTime.sql
@@ -12,6 +12,42 @@ CREATE TABLE TimeFact(
 	Second SMALLINT NULL
 );
 
+/*
+WITH RECURSIVE seconds(s) AS (
+    SELECT 0
+    
+    UNION ALL
+    
+    SELECT s + 1
+    FROM seconds
+    WHERE s + 1 < 60        
+), minutes(mi,s) AS (
+
+    SELECT 0, s
+    FROM seconds
+
+    UNION ALL
+
+    SELECT mi + 1, s
+    FROM minutes
+    WHERE mi + 1 < 60
+
+), hours(h,mi,s) AS (
+    
+    SELECT 0 , mi, s
+    FROM minutes
+    
+    UNION ALL
+    
+    SELECT h + 1, mi, s
+    FROM hours
+    WHERE h + 1 < 24
+)
+SELECT *
+FROM hours
+*/
+
+
 /* Fill TimeFact with Data */
 WITH RECURSIVE minutes(mi,s) AS (
     SELECT 0, 0
