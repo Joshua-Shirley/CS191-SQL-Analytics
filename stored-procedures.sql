d1d5a9c0f2d9d08e48fcd0ddd951af9528d888ae
Joshua Shirley <52868171+Joshua-Shirley@users.noreply.github.com>
2022-07-08 22:43:39 -0600

    Update stored-procedures.sql

--- stored-procedures.sql
+++ stored-procedures.sql
@@ -109,3 +109,61 @@ BEGIN
 END;
 $BODY$
 
+CREATE OR REPLACE PROCEDURE public.DatesFromTables
+()
+LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+    low DATE;
+    high DATE;
+BEGIN
+
+    SELECT INTO low, high
+           MIN(min)::DATE
+         , MAX(max)::DATE
+    FROM (
+        SELECT MIN(last_update), MAX(last_update) FROM actor  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM store  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM address  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM category  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM city  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM country  
+            UNION ALL 
+        SELECT MIN(create_date), MAX(create_date) FROM customer  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM customer  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM film_actor  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM film_category  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM inventory  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM language  
+            UNION ALL 
+        SELECT MIN(rental_date), MAX(rental_date) FROM rental  
+            UNION ALL 
+        SELECT MIN(return_date), MAX(return_date) FROM rental  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM rental  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM staff  
+            UNION ALL 
+        SELECT MIN(payment_date), MAX(payment_date) FROM payment  
+            UNION ALL 
+        SELECT MIN(last_update), MAX(last_update) FROM film  
+            UNION ALL 
+        SELECT MIN(fulldate), MAX(fulldate) FROM datefact  
+            UNION ALL 
+        SELECT MIN(fulldate), MAX(fulldate) FROM dateview
+    ) D;
+    
+    CALL public.updateDateTable(low,high);
+    
+END;
+$BODY$;
