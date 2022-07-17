397bd4e9188332f6e3e38d4d31f2492109af115b
Joshua Shirley <52868171+Joshua-Shirley@users.noreply.github.com>
2022-07-17 13:37:00 -0600

    Create bulk-insert-csv.sql

---
+++ Bulk_Insert/bulk-insert-csv.sql
@@ -0,0 +1,12 @@
+COPY c_in( first_name, last_name, email, address_1, address_2 , city , state , postal_code , country , phone )
+FROM '\WGU\CS 191\customer_list_v1.csv'
+DELIMITER ','
+CSV HEADER;
+
+-- INSERT FROM URL SOURCE
+/*
+COPY c_in( first_name, last_name, email, address_1, address_2 , city , state , postal_code , country , phone )
+FROM PROGRAM 'curl "https://github.com/Joshua-Shirley/CS191-SQL-Analytics/blob/main/csv%20files/customer_list_v1.csv"'
+DELIMITER ','
+CSV HEADER;
+*/
