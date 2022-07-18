4ce7cafc5a7a231dea06398e3e51566497d14e94
Joshua Shirley <52868171+Joshua-Shirley@users.noreply.github.com>
2022-07-17 20:58:24 -0600

    Update customer.sql

--- Bulk_Insert/customer.sql
+++ Bulk_Insert/customer.sql
@@ -19,4 +19,20 @@ AND CTE.first_name = customer.first_name
 WHERE customer_id IS NULL;
 
 
--- 
+-- UPDATE THE C_IN TABLE
+
+WITH CTE AS (
+    SELECT id
+    ,   customer.customer_id
+    FROM c_in
+    LEFT JOIN customer
+    ON c_in.address_id = customer.address_id
+    AND c_in.email = customer.email
+    AND c_in.last_name = customer.last_name
+    AND c_in.first_name = customer.first_name
+    WHERE c_in.customer_id IS NULL
+)
+UPDATE c_in
+SET customer_id = CTE.customer_id
+FROM CTE
+WHERE c_in.id = CTE.id;
