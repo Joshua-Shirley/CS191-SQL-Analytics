6edcae87d72f4cc855a9261aa2afa3b44b3124d3
Joshua Shirley <52868171+Joshua-Shirley@users.noreply.github.com>
2022-07-19 11:58:36 -0600

    Update Pivot-Film-Category-Month.sql

--- Reports/Pivot-Film-Category-Month.sql
+++ Reports/Pivot-Film-Category-Month.sql
@@ -1,3 +1,5 @@
+DROP VIEW Report_Pivot_Film_Category_Month;
+
 CREATE VIEW Report_Pivot_Film_Category_Month
 AS
 WITH CTE AS (
@@ -23,21 +25,35 @@ WITH CTE AS (
     GROUP BY month
 )
 , rentals AS (
-SELECT reports_rental_dates.name,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 1) AS january,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 2) AS febuary,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 3) AS march,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 4) AS april,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 5) AS may,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 6) AS june,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 7) AS july,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 8) AS august,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 9) AS september,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 10) AS october,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 11) AS november,
-    count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 12) AS december
-   FROM reports_rental_dates
-  GROUP BY reports_rental_dates.name
+    SELECT 
+        reports_rental_dates.name
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 1) AS January
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 2) AS Febuary
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 3) AS March
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 4) AS April
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 5) AS May
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 6) AS June
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 7) AS July
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 8) AS August
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 9) AS September
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 10) AS October
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 11) AS November
+    ,   count(reports_rental_dates.days_rented) FILTER (WHERE reports_rental_dates.month = 12) AS December
+    FROM reports_rental_dates
+    GROUP BY reports_rental_dates.name
 ) 
-SELECT *
+SELECT 
+    rentals.name as "Film Category"
+,   rentals.January
+,   rentals.Febuary
+,   rentals.March
+,   rentals.April
+,   rentals.May
+,   rentals.June
+,   rentals.July
+,   rentals.August
+,   rentals.September
+,   rentals.October
+,   rentals.November
+,   rentals.December
 FROM rentals;
