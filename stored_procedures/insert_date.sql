0435b9a579d3d36fce651231f0b5fa4c0ba90b08
Joshua Shirley <52868171+Joshua-Shirley@users.noreply.github.com>
2022-07-20 13:03:52 -0600

    Create insert_date.sql

---
+++ stored_procedures/insert_date.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE PROCEDURE public.insert_date(
+	IN d date)
+LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE 
+    DDay INT; 
+    DMonth INT;
+    DYear INT;
+    DId INT;
+BEGIN
+    
+    SELECT DATE_PART('YEAR',D) INTO DYear;
+    SELECT DATE_PART('MONTH',D) INTO DMonth;
+    SELECT DATE_PART('DAY',D) INTO DDay;
+        
+    SELECT COUNT(dateid) INTO DId
+    FROM datefact
+    WHERE Year = DYear AND Month = DMonth AND Day = DDay;
+            
+    IF DId = 0 THEN    
+        
+        INSERT INTO datefact
+        ( fulldate, year , month , day)
+        VALUES
+        ( fn_date_from_parts(DYear,DMonth,DDay) , DYear, DMonth, DDay );
+       
+    END IF;
+        
+END;
+$BODY$;
+ALTER PROCEDURE public.insert_date(date)
+    OWNER TO postgres;
