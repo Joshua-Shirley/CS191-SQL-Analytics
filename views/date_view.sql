a41e23d2d6eaaab65db939d0b52a587b50d1ddfd
Joshua Shirley <52868171+Joshua-Shirley@users.noreply.github.com>
2022-07-20 13:39:19 -0600

    Update date_view.sql

--- views/date_view.sql
+++ views/date_view.sql
@@ -1,16 +1,15 @@
-CREATE OR REPLACE VIEW public.date_view
+CREATE OR REPLACE VIEW public.dateview
  AS
- SELECT datefact.dateid,
-    datefact.fulldate,
-    datefact.year,
-    datefact.month,
-    datefact.day,    
-    EXTRACT( Quarter FROM fulldate ) as Quarter,
-    to_char(datefact.fulldate::timestamp with time zone, 'Month'::text) AS MonthName,    
-    EXTRACT( DOW FROM fulldate ) as DOW,
-    to_char(datefact.fulldate::timestamp with time zone, 'Weekday'::text) AS Weekday
-    
-   FROM datefact;
+    SELECT datefact.dateid
+        ,   datefact.fulldate
+        ,   datefact.year
+        ,   datefact.month
+        ,   datefact.day   
+        ,   EXTRACT( Quarter FROM fulldate ) as Quarter
+        ,   to_char(datefact.fulldate::timestamp with time zone, 'Month'::text) AS MonthName
+        ,   EXTRACT( DOW FROM fulldate ) as DOW
+        ,   to_char(datefact.fulldate::timestamp with time zone, 'Day'::text) AS Weekday   
+    FROM datefact;
 
-ALTER TABLE public.date_view
+ALTER TABLE public.dateview
     OWNER TO postgres;
