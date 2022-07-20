4b8d9e6ea176cc78edc4c62656ec2848d671ae1c
Joshua Shirley <52868171+Joshua-Shirley@users.noreply.github.com>
2022-07-20 14:51:37 -0600

    Update report_rental_detail.sql

--- table/report_rental_detail.sql
+++ table/report_rental_detail.sql
@@ -1,14 +1,15 @@
 CREATE TABLE report_rental_detail (
         rental_id INTEGER PRIMARY KEY
-    ,   inventory_id INTEGER
-    ,   film_id INTEGER
     ,   rental_date_id INTEGER
     ,   rental_time_id INTEGER
     ,   return_date_id INTEGER
     ,   return_time_id INTEGER
-    ,   store_id INTEGER
-    ,   category_id INTEGER
-    
+    ,   customer_id INTEGER        
+    ,   staff_id INTEGER
+    ,   store_id INTEGER        
+    ,   inventory_id INTEGER
+    ,   category_id INTEGER         
+    ,   film_id INTEGER 
 );
 
 ALTER TABLE IF EXISTS public.report_rental_detail
